6da937d6d059dc0e4b51631824414b6a65f8f823
JonelBriones <jonel.c.briones@gmail.com>
2025-01-26 21:43:35 -0800

    test on redirect

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,26 @@ import connectDB from "@/config/database";
 import User from "@/models/User";
 import { auth } from "./api/auth/[...nextauth]/auth";
 import { redirect } from "next/navigation";
+import Link from "next/link";
 
 export default async function Home() {
-  await connectDB();
-  const session = await auth();
+  // await connectDB();
+  // const session = await auth();
 
-  console.log("checking auth...");
-  if (!session || !session?.user?.id) {
-    console.log("not authenticated, redirecting to login.");
-    redirect("/login");
-  }
+  // console.log("checking auth...");
+  // if (!session || !session?.user?.id) {
+  //   console.log("not authenticated, redirecting to login.");
+  //   redirect("/login");
+  // }
+  return (
+    <div>
+      <h1>hello word</h1>
+      <Link href={"/login"}>go to login</Link>
+    </div>
+  );
 
-  const userApi = await User.findOne({ _id: session?.user?.id });
-  let notesApi = JSON.parse(JSON.stringify(userApi?.notes || []));
-  let user = JSON.parse(JSON.stringify(userApi));
-  return <Dashboard notesApi={notesApi} settings={user.settings} />;
+  // const userApi = await User.findOne({ _id: session?.user?.id });
+  // let notesApi = JSON.parse(JSON.stringify(userApi?.notes || []));
+  // let user = JSON.parse(JSON.stringify(userApi));
+  // return <Dashboard notesApi={notesApi} settings={user.settings} />;
 }
