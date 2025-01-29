678ac20a35b34c2a909435614d45140d7d0cecac
JonelBriones <jonel.c.briones@gmail.com>
2025-01-29 15:21:48 -0800

    move toast container to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import ContextWrapper, { useAppContext } from "@/components/Providers";
 import MobileNavbar from "@/components/mobile/MobileNavbar";
 import AuthProvider from "@/components/AuthProvider";
-
+import { ToastContainer } from "react-toastify";
 export const metadata: Metadata = {
   title: "Noted",
   description: "Note taking app",
@@ -20,6 +20,7 @@ export default async function RootLayout({
         <html lang="en" id="dark">
           <body className="mx-auto flex flex-col h-screen md:px-6 dark:bg-stone-900 dark:text-neutral-200 dark-transition relative overflow-hidden md:overflow-auto">
             {children}
+            <ToastContainer autoClose={3000} />
           </body>
         </html>
       </ContextWrapper>
--- components/forms/Signup.tsx
+++ components/forms/Signup.tsx
@@ -85,6 +85,7 @@ const Signup = ({ user }: any) => {
     });
     setSuccess(true);
     toast.success("You successfully signed up!");
+    redirect("/login");
   };
 
   // useEffect(() => {
@@ -97,7 +98,6 @@ const Signup = ({ user }: any) => {
 
   return (
     <div className="m-auto md:w-[540px] h-full p-8 w-full flex gap-4 flex-col justify-start">
-      <ToastContainer autoClose={3000} />
       <Link href={"/"} className="flex justify-center">
         <Image
           src={"/images/logo.svg"}
@@ -114,199 +114,183 @@ const Signup = ({ user }: any) => {
         </p>
       </div>
 
-      {success ? (
-        <div className="flex flex-col place-items-center gap-14 h-20 ">
-          <span className="relative">
-            <span className="absolute right-[24px]">
-              {/* <ReactLoader color={"#335CFF"} loading={success} /> */}
+      <form className="flex flex-col gap-4" onSubmit={onSubmitHandler}>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="email">Email Address</label>
+          <input
+            type="email"
+            autoComplete="email"
+            onChange={(e) => onChangeHandler(e)}
+            id="email"
+            name="email"
+            placeholder="email@example.com"
+            className={`p-2 rounded-lg border-2 border-neutral-300 text-neutral-500 text-sm hover:bg-neutral-50 outline-none  ${
+              errorMsg.emailError
+                ? "border-red-500"
+                : "outline-offset-2 focus:border-neutral-600 focus:ring-neutral-600 focus:outline-neutral-500"
+            }`}
+          />
+          {errorMsg.emailError && (
+            <span className="text-red-500 text-[12px] flex gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="#fb3748"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                  d="M3 12a9 9 0 1 0 18 0 9 9 0 0 0-18 0ZM12.006 15.693v-4.3M12 8.355v-.063"
+                />
+              </svg>
+              {errorMsg.emailError}
             </span>
-          </span>
-          <button
-            className="bg-blue-500 text-white flex place-items-center justify-center gap-4 p-2  border-2 rounded-lg cursor-pointer hover:bg-blue-700"
-            onClick={redirect("/login")}
-          >
-            Returning to Login Page
-          </button>
+          )}
         </div>
-      ) : (
-        <form className="flex flex-col gap-4" onSubmit={onSubmitHandler}>
-          <div className="flex flex-col gap-1">
-            <label htmlFor="email">Email Address</label>
+        <div className="flex flex-col gap-1">
+          <label
+            htmlFor="password"
+            className="flex justify-between place-items-center"
+          >
+            <span>Password</span>
+          </label>
+          <div className="w-full relative">
             <input
-              type="email"
-              autoComplete="email"
+              id="password"
+              name={"password"}
+              autoComplete="new-password"
               onChange={(e) => onChangeHandler(e)}
-              id="email"
-              name="email"
-              placeholder="email@example.com"
-              className={`p-2 rounded-lg border-2 border-neutral-300 text-neutral-500 text-sm hover:bg-neutral-50 outline-none  ${
-                errorMsg.emailError
+              type={toggleHidePassword ? "password" : "text"}
+              className={`p-2 rounded-lg border-2 border-neutral-300 focus:border-neutral-600 text-neutral-500 text-sm hover:bg-neutral-50 outline-none outline-offset-2 focus:ring-neutral-600 focus:outline-neutral-500 w-full`}
+            />
+            <div onClick={() => setToggleHidePassword(!toggleHidePassword)}>
+              <Image
+                src={`/images/icon-${
+                  toggleHidePassword ? "hide" : "show"
+                }-password.svg`}
+                width={0}
+                height={0}
+                alt="icon-hide-password"
+                className="size-5 absolute right-4 top-[10px] cursor-pointer"
+              />
+            </div>
+          </div>
+          {errorMsg.passwordError && (
+            <span className="text-red-500 text-[12px] flex gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="#fb3748"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                  d="M3 12a9 9 0 1 0 18 0 9 9 0 0 0-18 0ZM12.006 15.693v-4.3M12 8.355v-.063"
+                />
+              </svg>
+              {errorMsg.passwordError}
+            </span>
+          )}
+        </div>
+        <div className="flex flex-col gap-1">
+          <label
+            htmlFor="confirmPassword"
+            className="flex justify-between place-items-center"
+          >
+            <span>Confirm Password</span>
+          </label>
+          <div className="w-full relative">
+            <input
+              id="confirmPassword"
+              onChange={(e) => onChangeHandler(e)}
+              name={"confirmPassword"}
+              autoComplete="new-password"
+              type={toggleHideConfirmPassword ? "password" : "text"}
+              className={`p-2 rounded-lg border-2 border-neutral-300 focus:border-neutral-600 text-neutral-500 text-sm hover:bg-neutral-50 outline-none outline-offset-2 focus:ring-neutral-600 focus:outline-neutral-500 w-full  ${
+                errorMsg.confirmPasswordError
                   ? "border-red-500"
                   : "outline-offset-2 focus:border-neutral-600 focus:ring-neutral-600 focus:outline-neutral-500"
               }`}
             />
-            {errorMsg.emailError && (
-              <span className="text-red-500 text-[12px] flex gap-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="18"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke="#fb3748"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.5"
-                    d="M3 12a9 9 0 1 0 18 0 9 9 0 0 0-18 0ZM12.006 15.693v-4.3M12 8.355v-.063"
-                  />
-                </svg>
-                {errorMsg.emailError}
-              </span>
-            )}
-          </div>
-          <div className="flex flex-col gap-1">
-            <label
-              htmlFor="password"
-              className="flex justify-between place-items-center"
+            <div
+              onClick={() =>
+                setToggleHideConfirmPassword(!toggleHideConfirmPassword)
+              }
             >
-              <span>Password</span>
-            </label>
-            <div className="w-full relative">
-              <input
-                id="password"
-                name={"password"}
-                autoComplete="new-password"
-                onChange={(e) => onChangeHandler(e)}
-                type={toggleHidePassword ? "password" : "text"}
-                className={`p-2 rounded-lg border-2 border-neutral-300 focus:border-neutral-600 text-neutral-500 text-sm hover:bg-neutral-50 outline-none outline-offset-2 focus:ring-neutral-600 focus:outline-neutral-500 w-full`}
+              <Image
+                src={`/images/icon-${
+                  toggleHideConfirmPassword ? "hide" : "show"
+                }-password.svg`}
+                width={0}
+                height={0}
+                alt="icon-hide-password"
+                className="size-5 absolute right-4 top-[10px] cursor-pointer"
               />
-              <div onClick={() => setToggleHidePassword(!toggleHidePassword)}>
-                <Image
-                  src={`/images/icon-${
-                    toggleHidePassword ? "hide" : "show"
-                  }-password.svg`}
-                  width={0}
-                  height={0}
-                  alt="icon-hide-password"
-                  className="size-5 absolute right-4 top-[10px] cursor-pointer"
-                />
-              </div>
             </div>
-            {errorMsg.passwordError && (
-              <span className="text-red-500 text-[12px] flex gap-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="18"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke="#fb3748"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.5"
-                    d="M3 12a9 9 0 1 0 18 0 9 9 0 0 0-18 0ZM12.006 15.693v-4.3M12 8.355v-.063"
-                  />
-                </svg>
-                {errorMsg.passwordError}
-              </span>
-            )}
           </div>
-          <div className="flex flex-col gap-1">
-            <label
-              htmlFor="confirmPassword"
-              className="flex justify-between place-items-center"
-            >
-              <span>Confirm Password</span>
-            </label>
-            <div className="w-full relative">
-              <input
-                id="confirmPassword"
-                onChange={(e) => onChangeHandler(e)}
-                name={"confirmPassword"}
-                autoComplete="new-password"
-                type={toggleHideConfirmPassword ? "password" : "text"}
-                className={`p-2 rounded-lg border-2 border-neutral-300 focus:border-neutral-600 text-neutral-500 text-sm hover:bg-neutral-50 outline-none outline-offset-2 focus:ring-neutral-600 focus:outline-neutral-500 w-full  ${
-                  errorMsg.confirmPasswordError
-                    ? "border-red-500"
-                    : "outline-offset-2 focus:border-neutral-600 focus:ring-neutral-600 focus:outline-neutral-500"
-                }`}
-              />
-              <div
-                onClick={() =>
-                  setToggleHideConfirmPassword(!toggleHideConfirmPassword)
-                }
+          {errorMsg.confirmPasswordError && (
+            <span className="text-red-500 text-[12px] flex gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                fill="none"
+                viewBox="0 0 24 24"
               >
-                <Image
-                  src={`/images/icon-${
-                    toggleHideConfirmPassword ? "hide" : "show"
-                  }-password.svg`}
-                  width={0}
-                  height={0}
-                  alt="icon-hide-password"
-                  className="size-5 absolute right-4 top-[10px] cursor-pointer"
+                <path
+                  stroke="#fb3748"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                  d="M3 12a9 9 0 1 0 18 0 9 9 0 0 0-18 0ZM12.006 15.693v-4.3M12 8.355v-.063"
                 />
-              </div>
-            </div>
-            {errorMsg.confirmPasswordError && (
-              <span className="text-red-500 text-[12px] flex gap-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="18"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke="#fb3748"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.5"
-                    d="M3 12a9 9 0 1 0 18 0 9 9 0 0 0-18 0ZM12.006 15.693v-4.3M12 8.355v-.063"
-                  />
-                </svg>
-                {errorMsg.confirmPasswordError}
-              </span>
-            )}
-          </div>
-          <div className="flex flex-col gap-6 text-center">
-            <button className="bg-blue-500 text-white flex place-items-center justify-center gap-4 p-2  border-2 rounded-lg cursor-pointer hover:bg-blue-700">
-              Signup
-            </button>
-            <div className="w-full h-[1px] bg-neutral-200" />
-            <div className="flex flex-col gap-4">
-              <p className="text-neutral-600 text-sm">Or log in with:</p>
-              <button
-                className="flex place-items-center justify-center gap-4 p-2  border-2 rounded-lg cursor-pointer hover:bg-neutral-50"
-                onClick={() => signIn("google")}
-              >
-                <Image
-                  src={"/images/icon-google.svg"}
-                  width={0}
-                  height={0}
-                  alt="icon-google"
-                  className="size-[25px]"
-                />
-                <span className="text-lg text-neutral-950 font-medium">
-                  Google
-                </span>
-              </button>
-            </div>
-            <span className="text-neutral-600 text-sm">
-              Already have an account?{" "}
-              <Link
-                href={"/login"}
-                className="text-neutral-950 hover:text-blue-500"
-              >
-                Login
-              </Link>
+              </svg>
+              {errorMsg.confirmPasswordError}
             </span>
+          )}
+        </div>
+        <div className="flex flex-col gap-6 text-center">
+          <button className="bg-blue-500 text-white flex place-items-center justify-center gap-4 p-2  border-2 rounded-lg cursor-pointer hover:bg-blue-700">
+            Signup
+          </button>
+          <div className="w-full h-[1px] bg-neutral-200" />
+          <div className="flex flex-col gap-4">
+            <p className="text-neutral-600 text-sm">Or log in with:</p>
+            <button
+              className="flex place-items-center justify-center gap-4 p-2  border-2 rounded-lg cursor-pointer hover:bg-neutral-50"
+              onClick={() => signIn("google")}
+            >
+              <Image
+                src={"/images/icon-google.svg"}
+                width={0}
+                height={0}
+                alt="icon-google"
+                className="size-[25px]"
+              />
+              <span className="text-lg text-neutral-950 font-medium">
+                Google
+              </span>
+            </button>
           </div>
-        </form>
-      )}
+          <span className="text-neutral-600 text-sm">
+            Already have an account?{" "}
+            <Link
+              href={"/login"}
+              className="text-neutral-950 hover:text-blue-500"
+            >
+              Login
+            </Link>
+          </span>
+        </div>
+      </form>
     </div>
   );
 };
